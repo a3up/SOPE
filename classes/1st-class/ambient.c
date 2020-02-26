8f6a6d1d9f8a0d3d4f81545d71256c47158471fe
a3brx <a3brx@sapo.pt>
2020-02-26 21:57:45 +0000

    Fix

--- classes/1st-class/ambient.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv, char **envp) {
-    unsigned char i = 0;
-    while(envp[i] != NULL)
-        printf("%s\n", envp[i++]);
-    return 0;
-}
--- classes/1st-class/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(void) {
-    printf("Hello %s!\n", getenv("USER_NAME"));
-    return 0;
-}
